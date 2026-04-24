64b979021829c586692377d5aaaf5661797d4642
Boybash <toheebibidapo@gmail.com>
2026-04-24 10:05:21 +0100

    button corrected

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -1,7 +1,6 @@
 export default function Button({ children, onClick, className }) {
   return (
     <button
-      className="bg-[#571244] font-semibold text-[18px]"
       onClick={onClick}
       className={`bg-[#571244] font-semibold text-[18px] ${className || ""}`}
     >
